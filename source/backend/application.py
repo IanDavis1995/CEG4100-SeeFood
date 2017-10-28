b9cb4206ec420800b99a53bca86ce157bc4424dd
Ian <davis.835@wright.edu>
2017-10-28 13:10:23 -0400

    Android app initial commit

--- source/backend/application.py
+++ source/backend/application.py
@@ -1,7 +1,18 @@
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/gallery")
+def get_gallery():
     return "Hello World!"
+
+
+@app.route("/analyze")
+def analyze():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
