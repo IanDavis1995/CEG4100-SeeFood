779a54aa3739a8f381cd95fa990ab75590d89f0b
aaronkoenig11 <31825725+aaronkoenig11@users.noreply.github.com>
2017-11-30 12:55:30 -0500

    Update application.py
    Final Changes

--- source/backend/application.py
+++ source/backend/application.py
@@ -41,9 +41,10 @@ def parseJSON():
     name = dataDict['name']
     classType = dataDict['food']
     timePeriod = dataDict['time']
+    imgType = dataDict['type']
 
     # format and execute query
-    query = queryGenerator(name, classType, timePeriod)
+    query = queryGenerator(name, classType, timePeriod, imgType)
     # return query
     cur.execute(query)
 
@@ -54,6 +55,7 @@ def parseJSON():
     for result in cur.fetchall():
         jsonObj[str('file%s' % i)] = {}
         jsonObj[str('file%s' % i)]['name'] = result[0]
+        # int(result[1].encode("hex"))
         jsonObj[str('file%s' % i)]['contains_food'] = result[1] == "\x01"
         jsonObj[str('file%s' % i)]['certainty'] = result[4]
         jsonObj[str('file%s' % i)]['type'] = 'jpg'
@@ -68,10 +70,14 @@ def parseJSON():
     return json.dumps(jsonObj)
 
 
-def queryGenerator(name, classType, timePeriod):
+def queryGenerator(name, classType, timePeriod, imgType):
     # in progress
     today = datetime.datetime.now().date()
-    query = "SELECT name, class, upload_time, img_pth, confidence FROM imageuploads WHERE "
+    quer = ""
+    if imgType == "T":
+      query = "SELECT name, class, upload_time, thumbnail_pth, confidence FROM imageuploads WHERE "
+    else:
+      query = "SELECT name, class, upload_time, img_pth, confidence FROM imageuploads WHERE " 
 
     if name:
         query += "name='%s' " % name
@@ -126,7 +132,9 @@ def analyze_json():
 
     img_tensor = [np.asarray(image, dtype=np.float32)]
     scores = sess.run(class_scores, {x_input: img_tensor, keep_prob: 1.})
-    confidence = abs(scores[0][0] - scores[0][1]) / 5 * 100
+    confidence = abs(scores[0][0] - scores[0][1]) / 4 * 100
+    if confidence > 100:
+      confidence = 100.0
 
     # connect to DB
     myConnection = pymysql.connect(user='root', host='localhost', db='seefood')
@@ -144,56 +152,60 @@ def analyze_json():
         "name": filename,
         "type": filetype,
         "contains_food": not np.argmax(scores) == 1,
-        "certainty": confidence
+        "certainty": '{0:.2f}'.format(confidence)
     })
-
 
 @app.route("/analyze", methods=['POST'])
 def analyze():
     if 'file' not in request.files:
-        return "No File Found!"
+      return "No File Found!"
+    uploaded_files = request.files.getlist("file")
+    jsonResults = []
+    for file in uploaded_files:
+      uploaded_file = file
+      path_name = os.path.join(APP_HOME, 'uploaded_images/', str(uploaded_file.filename))
+      uploaded_file.save(path_name)
+      os.chmod(path_name, 0777)
 
-    uploaded_file = request.files['file']
-    path_name = os.path.join(APP_HOME, 'uploaded_images/', str(uploaded_file.filename))
-    uploaded_file.save(path_name)
-    os.chmod(path_name, 0777)
+      image_path = path_name
+      image = Image.open(image_path).convert('RGB')
+      image = image.resize((227, 227), Image.ANTIALIAS)
+      resized_path = os.path.join(APP_HOME, 'resized_images/', path_name.split('/')[-1])
+      image.save(resized_path, "JPEG")
 
-    image_path = path_name
-    image = Image.open(image_path).convert('RGB')
-    image = image.resize((227, 227), Image.ANTIALIAS)
-    resized_path = os.path.join(APP_HOME, 'resized_images/', path_name.split('/')[-1])
-    image.save(resized_path, "JPEG")
+      # Work in RGBA space (A=alpha) since png's come in as RGBA, jpeg come in as RGB
+      # so convert everything to RGBA and then to RGB.
+      image_path = path_name
+      image = Image.open(image_path).convert('RGB')
+      image = image.resize((227, 227), Image.BILINEAR)
+      img_tensor = [np.asarray(image, dtype=np.float32)]
 
-    # Work in RGBA space (A=alpha) since png's come in as RGBA, jpeg come in as RGB
-    # so convert everything to RGBA and then to RGB.
-    image_path = path_name
-    image = Image.open(image_path).convert('RGB')
-    image = image.resize((227, 227), Image.BILINEAR)
-    img_tensor = [np.asarray(image, dtype=np.float32)]
+      # Run the image in the model.
+      scores = sess.run(class_scores, {x_input: img_tensor, keep_prob: 1.})
+      confidence = abs(scores[0][0] - scores[0][1]) / 4 * 100
+      if confidence > 100:
+        confidence = 100.0
 
-    # Run the image in the model.
-    scores = sess.run(class_scores, {x_input: img_tensor, keep_prob: 1.})
-    confidence = abs(scores[0][0] - scores[0][1]) / 5 * 100
+      # connect to DB
+      myConnection = pymysql.connect(user='root', host='localhost', db='seefood')
+      cur = myConnection.cursor()
 
-    # connect to DB
-    myConnection = pymysql.connect(user='root', host='localhost', db='seefood')
-    cur = myConnection.cursor()
+      user = "user"
+      date = datetime.datetime.now()
+      classType = int(not np.argmax(scores) == 1)
+      name = uploaded_file.filename.split(".")[-2]
 
-    user = "user"
-    date = datetime.datetime.now()
-    classType = int(not np.argmax(scores) == 1)
-    name = uploaded_file.filename.split(".")[-2]
+      query = "INSERT INTO imageuploads (name, class, upload_time, user, confidence, thumbnail_pth, img_pth) VALUES ('%s', %s, '%s', '%s', %f, '%s', '%s')" % (name, classType, date, user, confidence, image_path, image_path)
+      cur.execute(query)
+      myConnection.commit()
 
-    query = "INSERT INTO imageuploads (name, class, upload_time, user, confidence, thumbnail_pth, img_pth) VALUES ('%s', %s, '%s', '%s', %f, '%s', '%s')" % (name, classType, date, user, confidence, image_path, image_path)
-    cur.execute(query)
-    myConnection.commit()
-
-    return json.dumps({
+      jsonResults.append({
         "name": uploaded_file.filename,
         "type": "jpg",
         "contains_food": not np.argmax(scores) == 1,
-        "certainty": confidence
-    })
+        "certainty": '{0:.2f}'.format(confidence)
+      })
+    return json.dumps(jsonResults)
 
 
 if __name__ == "__main__":
